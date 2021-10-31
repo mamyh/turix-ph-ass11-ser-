e56ace032333c6241a109a6605abe1f4e11a9e85
mamyh <likhmaon@gmail.com>
2021-10-31 14:51:59 +0600

    cors updated

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 const port = process.env.PORT || 5000;
 //middleware
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const run = async () => {
